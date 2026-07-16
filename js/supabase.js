e533d58240b1f572bfc2016b326540df91f8cd0e
Nathy <tinathy08@hotmail.com>
2026-07-16 16:10:19 +0200

    tes test supabase

--- js/supabase.js
+++ js/supabase.js
@@ -2,13 +2,13 @@ const SUPABASE_URL = "https://mgvfougddqbhpufnouom.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_4XP5smvqkCD7mB6OQt3dtg_l0GR360M";
 
-const supabase = window.supabase.createClient(
+const db = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
 
 async function testConnexion() {
-    const { data, error } = await supabase
+    const { data, error } = await db
         .from("Créations")
         .select("*")
         .eq("slug", "dragon-fusion");
