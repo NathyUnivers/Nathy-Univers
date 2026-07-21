d60e7a15940491d30422251698f8031dedfd7c85
Nathy <tinathy08@hotmail.com>
2026-07-21 12:26:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -323,7 +323,7 @@ async function chargerCommentaires(slug, commentaires) {
         .from("commentaires")
         .select("*")
         .eq("slug", slug)
-        .order("created_at", { ascending: true });
+        .order("created_at", { ascending: false });
 
         console.log("Commentaires récupérés :", data);
 
